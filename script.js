1e35fcf6828964b4243df106c1fed31817f076d4
Alex Khoa Le <leduykhoa@hotmail.ca>
2019-10-17 00:56:45 -0400

    Attached functions to submit button

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ function returnCurrentWeather(cityName) {
         let weatherIcon = `http://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png`;
         
         currWeatherDiv.html(`
-        <h2>${response.name} (${currTime.getMonth()}/${currTime.getDate()}/${currTime.getFullYear()}) <img src=${weatherIcon} height="70px"></h2>
+        <h2>${response.name}, ${response.sys.country} (${currTime.getMonth()}/${currTime.getDate()}/${currTime.getFullYear()})<img src=${weatherIcon} height="70px"></h2>
         <p>Temperature: ${response.main.temp} &#176;C</p>
         <p>Humidity: ${response.main.humidity}%</p>
         <p>Wind Speed: ${response.wind.speed} m/s</p>
@@ -30,8 +30,6 @@ function returnWeatherForecast(cityName) {
             }
             let forecastDate = new Date(forecastInfo[i].dt*1000);
             let weatherIcon = `http://openweathermap.org/img/wn/${forecastInfo[i].weather[0].icon}.png`;
-            console.log(forecastInfo[i].weather[0].icon)
-
 
             forecastDiv.append(`
             <div class="col-md">
@@ -48,3 +46,10 @@ function returnWeatherForecast(cityName) {
         })
     })
 };
+
+$("#submitCity").click(function() {
+    event.preventDefault();
+    let cityName = $("#cityInput").val();
+    returnCurrentWeather(cityName);
+    returnWeatherForecast(cityName);
+})
