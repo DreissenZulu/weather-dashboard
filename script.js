68e3d4da4f2c54aad453decdb8c7648c56c5ee2b
Alex Khoa Le <leduykhoa@hotmail.ca>
2019-10-16 22:04:41 -0400

    Further defined the current weather script; initialized forecast function

--- script.js
+++ script.js
@@ -7,6 +7,22 @@ function returnCurrentWeather(cityName) {
 
     $.get(queryURL).then(function(response){
         console.log(response);
+        let unixTime = new Date();
+
+        currWeatherDiv.html(`
+        <h2>${response.name} (${unixTime.getMonth()}/${unixTime.getDate()}/${unixTime.getFullYear()})</h2>
+        <p>Temperature: ${(response.main.temp - 273.15).toFixed(1)} &#176;C</p>
+        <p>Humidity: ${response.main.humidity}%</p>
+        <p>Wind Speed: ${response.wind.speed} m/s</p>
+        `)
+    })
+};
+
+function returnWeatherForecast(cityName) {
+    let queryURL = `http://api.openweathermap.org/data/2.5/forecast?q=${cityName}&APPID=${apiKey}`;
+
+    $.get(queryURL).then(function(response){
+        console.log(response);
         currWeatherDiv.html(`
         <h2>${response.name}</h2>
         <p>Temperature: ${(response.main.temp - 273.15).toFixed(1)} &#176;C</p>
