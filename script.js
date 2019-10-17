c09c96430f10a08bb334a36dd4bb9d2fca3346ba
Alex Khoa Le <leduykhoa@hotmail.ca>
2019-10-17 15:49:40 -0400

    Month now shows the right number

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ function returnCurrentWeather(cityName) {
         let weatherIcon = `https://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png`;
 
         currWeatherDiv.html(`
-        <h2>${response.name}, ${response.sys.country} (${currTime.getMonth()}/${currTime.getDate()}/${currTime.getFullYear()})<img src=${weatherIcon} height="70px"></h2>
+        <h2>${response.name}, ${response.sys.country} (${currTime.getMonth()+1}/${currTime.getDate()}/${currTime.getFullYear()})<img src=${weatherIcon} height="70px"></h2>
         <p>Temperature: ${response.main.temp} &#176;C</p>
         <p>Humidity: ${response.main.humidity}%</p>
         <p>Wind Speed: ${response.wind.speed} m/s</p>
@@ -45,7 +45,7 @@ function returnWeatherForecast(cityName) {
             <div class="col-md">
                 <div class="card text-white bg-primary">
                     <div class="card-body">
-                        <h4>${forecastDate.getMonth()}/${forecastDate.getDate()}/${forecastDate.getFullYear()}</h4>
+                        <h4>${forecastDate.getMonth()+1}/${forecastDate.getDate()}/${forecastDate.getFullYear()}</h4>
                         <img src=${weatherIcon} alt="Icon">
                         <p>Temp: ${forecastInfo[i].main.temp} &#176;C</p>
                         <p>Humidity: ${forecastInfo[i].main.humidity}%</p>
